fc72441e021e55d6a4742d75dca39d83acb75a22
pid011 <pid011@kakao.com>
2020-10-05 17:39:36 +0900

    Add chapter1_5 & Remove .vscode configs

---
+++ src/chapter1_5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int add_two_numbers(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
+int main(int, char**) {
+    cout << add_two_numbers(1, 2) << endl;
+    cout << add_two_numbers(3, 4) << endl;
+    cout << add_two_numbers(5, 6) << endl;
+}
