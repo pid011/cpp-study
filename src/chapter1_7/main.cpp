0048fa1a9ddd6e0ec90bc738c174d7d46296fc5e
pid011 <pid011@kakao.com>
2020-10-06 02:12:00 +0900

    Add chapter1_7

---
+++ src/chapter1_7/main.cpp
@@ -0,0 +1,17 @@
+﻿// 지역 범위
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 0;
+    cout << x << " " << &x << endl;
+    {
+        int x = 1; // 지역 밖으로 나가면 사용 불가능
+        cout << x << " " << &x << endl;
+    }
+    // 밖에 있는 x와 안에 있는 x의 주소가 다르다.
+
+    { int x = 2; }
+}
