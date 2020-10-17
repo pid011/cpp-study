47d82f888ea4a46782899db28d8f28effbee1ef5
pid011 <pid011@kakao.com>
2020-10-17 14:55:14 +0900

    Add chapter2_3

---
+++ src/chapter2_3/main.cpp
@@ -0,0 +1,15 @@
+// chapter2_3: Fixed-width Integers
+
+#include <iostream>
+// #include <cstdint> // iostream이 cstdint를 선언함
+
+int main(int, char**) {
+    using namespace std;
+
+    int16_t i(5);
+    int8_t myint(65); // char로 교체하기 때문에 결국엔 char이랑 같다.
+    cout << myint << endl;
+
+    int_fast32_t fi(5);  // 32비트 데이터타입 중 빠른 타입
+    int_least64_t fl(5); // 적어도 64비트인 데이터타입
+}
