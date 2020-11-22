8ca6d30c0cd1d96ce16750eb741ef62d1e808e63
pid011 <pid011@kakao.com>
2020-11-22 20:44:09 +0900

    Add chapter03_04

--- src/chapter03_04/main.cpp
+++ src/chapter03_04/main.cpp
@@ -1,20 +1,43 @@
-// chapter03_04.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿// sizeof, comma operator, conditional operator
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    {
+        float a { 1 };
+        // sizeof는 함수가 아니라 연산자이다.
+        cout << sizeof(float) << endl;
+        cout << sizeof(a) << endl;
+        cout << sizeof a << endl;
+    }
+    cout << endl;
+    {
+        // comma operator
+        int x { 3 };
+        int y { 10 };
+        int z = (++x, ++y); // ++x; ++y; int z = y;
+
+        cout << x << " " << y << " " << z << endl;
+    }
+    cout << endl;
+    {
+        int a = 1, b = 10;
+        int z;
+        // comma의 우선순위가 assignment보다 낮아서 assignment가 먼저 실행됨
+        z = a, b;
+        cout << z << endl; // 1
+
+        z = (++a, a + b);  // ++a = 2, z = 2 + 10
+        cout << z << endl; // 12
+    }
+    cout << endl;
+    {
+        // conditional operator (arithmetric if)
+
+        bool on_sale = true;
+        // 삼항연산자는 대입할 변수가 const여도 사용가능
+        const int price = on_sale ? 10 : 100;
+
+        cout << price << endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
