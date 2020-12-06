f84577d10e04262c2daf96a4b154c2b8afb4d354
pid011 <pid011@kakao.com>
2020-12-07 02:42:21 +0900

    feat: add chapter04_09

---
+++ src/chapter04_09/chapter04_09.cpp
@@ -0,0 +1,18 @@
+﻿// Type aliases
+#include <iostream>
+#include <vector>
+
+int main() {
+    using namespace std;
+
+    typedef double distance_t;
+
+    double my_distance {};
+    distance_t home2work {};
+
+    // typedef vector<pair<string, int>> pairlist_t;
+    using pairlist_t = vector<pair<string, int>>;
+
+    pairlist_t pairlist1;
+    pairlist_t pairlist2;
+}
