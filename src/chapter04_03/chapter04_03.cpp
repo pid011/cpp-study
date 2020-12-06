3f6b75accf1387526f2e34164ba1e9968fe8de68
pid011 <pid011@kakao.com>
2020-12-06 19:41:42 +0900

    Add chapter04_03

---
+++ src/chapter04_03/chapter04_03.cpp
@@ -0,0 +1,21 @@
+﻿// using, Ambiguity
+#include <iostream>
+
+namespace a {
+    int my_a {10};
+    int count {3};
+} // namespace a
+
+namespace b {
+    int my_a {10};
+}
+
+int main() {
+    // using namespace를 전역에 선언해버리면 좋지 않음
+    // header보다는 cpp파일에 넣는게 좋음
+    using namespace std;
+    using namespace a;
+
+    cout << my_a << endl;
+    // cout << count << endl; // std에도 count가 있어서 모호성이 생김
+}
