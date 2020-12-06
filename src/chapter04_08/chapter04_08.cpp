eaa578df30626b0f6eaf9488b883dd5c2f5c5fb4
pid011 <pid011@kakao.com>
2020-12-07 02:41:51 +0900

    Add chapter04_08

---
+++ src/chapter04_08/chapter04_08.cpp
@@ -0,0 +1,23 @@
+﻿// Scoped Enumerations (Enum Class)
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    enum class Color {
+        RED,
+        BLUE
+    };
+
+    enum class Fruit {
+        BANANA,
+        APPLE
+    };
+
+    Color color1 = Color::RED;
+    Color color2 = Color::RED;
+
+    if (color1 == color2) {
+        cout << "Same color" << endl;
+    }
+}
