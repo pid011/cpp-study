e646ac52b2c1c5d547a651db9f9df86a02e40a0e
pid011 <pid011@kakao.com>
2020-12-06 19:41:52 +0900

    Add chapter04_04

---
+++ src/chapter04_04/chapter04_04.cpp
@@ -0,0 +1,22 @@
+﻿// auto, type inference
+#include <iostream>
+
+auto add(int x, int y) {
+    return x + y;
+}
+
+// trailing return type / C++ 11
+auto add(double x, double y) -> double {
+    return x + y;
+}
+
+int main() {
+    using namespace std;
+
+    auto a = 123; // auto는 초기화와 함께 사용해야 함
+    auto d = 123.0;
+    auto c = 1 + 2;
+    auto result = add(1, 2);
+
+    return 0;
+}
