71ee4fb172b678a30c33b015f1fc8cad776bde17
pid011 <pid011@kakao.com>
2020-12-07 15:37:21 +0900

    feat: add chapter05_02

---
+++ src/chapter05_02/chapter05_02.cpp
@@ -0,0 +1,47 @@
+﻿// if
+#include <iostream>
+
+int min(int x, int y) {
+    return x < y ? x : y;
+}
+
+int main() {
+    using namespace std;
+
+    int x, y;
+    cin >> x >> y;
+
+    //if (x > 10) {
+    //    cout << x << " is greater than 10" << endl;
+    //} else {
+    //    cout << x << " is not greater than 10" << endl;
+    //}
+
+    //if (1) {
+    //    int x = 5;
+    //} else {
+    //    int x = 6;
+    //}
+    //cout << x << endl; // cin에서 입력한 수가 출력됨
+
+    //if (x >= 10) {
+    //    if (x <= 20) {
+    //        cout << "x is between 10 and 20" << endl;
+    //    } else {
+    //        cout << "x is greater than 20" << endl;
+    //    }
+    //}
+
+    //if (x > 0 && y > 0) {
+    //    cout << "both numbers are positive" << endl;
+    //} else if (x > 0 || y > 0) {
+    //    cout << "One of the numbers are positive" << endl;
+    //} else {
+    //    cout << "Neither number is positive" << endl;
+    //}
+
+    if (x = 0) {
+        cout << x << endl;
+    }
+    cout << x << endl;
+}
