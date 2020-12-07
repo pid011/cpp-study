c010d46ea276db3892575df6dff8be65555b1944
pid011 <pid011@kakao.com>
2020-12-07 15:37:09 +0900

    feat: add chapter05_01

---
+++ src/chapter05_01/chapter05_01.cpp
@@ -0,0 +1,16 @@
+﻿// Control flow
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout << "I love you" << endl;
+
+    //return 0;
+    exit(0); // 코드의 어디에 있든간에 프로그램 종료 가능
+
+    cout << "I love you, too" << endl;
+
+    return 0;
+}
