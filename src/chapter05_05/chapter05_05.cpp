b856c08ada79be4d652d0fd32ad73b8b53d95760
pid011 <pid011@kakao.com>
2021-01-05 19:16:10 +0900

    feat: add chapter05_05

---
+++ src/chapter05_05/chapter05_05.cpp
@@ -0,0 +1,39 @@
+﻿// while
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    //{
+    //    int count = 0;
+    //    while (count < 10) {
+    //        cout << count << endl;
+    //        ++count;
+    //    }
+    //}
+
+    //{
+    //    int count = 1;
+    //    while (count < 100) {
+    //        if (count % 5 == 0) {
+    //            cout << "Hello " << count << endl;
+    //        }
+    //        ++count;
+    //    }
+    //}
+
+    int i = 1;
+    while (i < 5)
+    {
+        int j = 1;
+        while (j < i + 1)
+        {
+            cout << j << " ";
+            ++j;
+        }
+
+        cout << endl;
+        ++i;
+    }
+}
