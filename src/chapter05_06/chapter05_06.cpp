4e69e8366fa2d373d661f4624320658e8f2a2c1d
pid011 <pid011@kakao.com>
2021-01-05 19:16:26 +0900

    feat: add chapter05_06

---
+++ src/chapter05_06/chapter05_06.cpp
@@ -0,0 +1,19 @@
+﻿// do-while
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int selection;
+    do
+    {
+        cout << "1. add" << endl;
+        cout << "2. sub" << endl;
+        cout << "3. mult" << endl;
+        cout << "4. div" << endl;
+        cin >> selection;
+    } while (selection <= 0 || selection >= 5);
+
+    cout << "You selected " << selection << "." << endl;
+}
