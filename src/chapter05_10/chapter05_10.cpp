c5d7445fe5917018ea361d2521411e896a48f3c6
pid011 <pid011@kakao.com>
2021-01-05 19:17:30 +0900

    feat: add chapter05_10

---
+++ src/chapter05_10/chapter05_10.cpp
@@ -0,0 +1,73 @@
+﻿// std::cin
+#include <iostream>
+
+using namespace std;
+
+int GetInt()
+{
+    while (true)
+    {
+        cout << "Enter an integer number: ";
+        int x;
+        cin >> x;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            cout << "Invalid number, please try again" << endl;
+            cin.ignore(32767, '\n');
+        }
+        else
+        {
+            cin.ignore(32767, '\n');
+            return x;
+        }
+    }
+}
+
+char GetOperator()
+{
+    while (true)
+    {
+        cout << "Enter an operator (+, -): ";
+        char op;
+        cin >> op;
+        cin.ignore(32767, '\n');
+
+        if (op == '+' || op == '-')
+        {
+            return op;
+        }
+        else
+        {
+            cout << "Invalid operator, please try again" << endl;
+        }
+    }
+}
+
+void PrintResult(int x, char op, int y)
+{
+    if (op == '+')
+    {
+        cout << x + y << endl;
+    }
+    else if (op == '-')
+    {
+        cout << x - y << endl;
+    }
+    else
+    {
+        cout << "Invalid operator" << endl;
+    }
+}
+
+int main()
+{
+    int x = GetInt();
+    char op = GetOperator();
+    int y = GetInt();
+
+    PrintResult(x, op, y);
+
+    return 0;
+}
