6b63391985514052ff8f394175acbfda2f602b32
pid011 <pid011@kakao.com>
2021-01-05 19:16:42 +0900

    feat: add chapter05_07

---
+++ src/chapter05_07/chapter05_07.cpp
@@ -0,0 +1,25 @@
+﻿// for loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //for (int count = 0; count < 10; ++count)
+    //{
+    //    cout << count << endl;
+    //}
+
+    //// 여러개의 변수 선언 가능
+    //for (int i = 0, j = 10; i < 10; ++i, --j)
+    //{
+    //    cout << i << " " << j << endl;
+    //}
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << i << " " << j << endl;
+        }
+    }
+}
