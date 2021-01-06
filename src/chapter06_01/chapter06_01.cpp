bf88d1121ef32c9f374a233cf425b9f4438f1333
Pilgyeong Gwak <pid011@kakao.com>
2021-01-06 17:02:00 +0900

    feat: add chapter06_01

---
+++ src/chapter06_01/chapter06_01.cpp
@@ -0,0 +1,25 @@
+﻿// array basic usage 1
+#include <iostream>
+
+using namespace std;
+
+enum Students {
+    JACK,
+    JAMES,
+    KAIN,
+    Kaiser,
+    STUDENTS_COUNT
+};
+
+int main() {
+    int arr[100] { 1, 2, 3 };
+    cout << arr[1] << endl;
+
+    int scores[STUDENTS_COUNT];
+    scores[JACK] = 65;
+    scores[JAMES] = 80;
+
+    cout << scores[JACK] << endl;
+
+    return 0;
+}
