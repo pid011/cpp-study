54975d9174c878484df1adbad8c47a56140f8a46
pid011 <pid011@kakao.com>
2021-01-29 17:55:39 +0900

    feat: add chapter06_05

---
+++ src/chapter06_05/chapter06_05.cpp
@@ -0,0 +1,34 @@
+﻿// Multi-dimensional Array
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int array[3][4] = {
+        { 1, 2 }, // 비어있는 부분은 기본값으로 채워짐
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 }
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << array[i][j] << '\t';
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << (int)&array[i][j] << '\t';
+        }
+        cout << endl;
+    }
+    //2차원이지만 메모리에서는 일렬로 나열됨
+}
