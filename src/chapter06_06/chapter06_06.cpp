e0b1bc595b4950ab7c7aae763bcd1b70fd7d3706
pid011 <pid011@kakao.com>
2021-02-04 14:10:28 +0900

    feat: add chapter06_06

---
+++ src/chapter06_06/chapter06_06.cpp
@@ -0,0 +1,38 @@
+﻿// c-style string array
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    //char str[] = "string";
+    //cout << sizeof(str) / sizeof(str[0]) << endl;
+
+    // 문자열의 끝에는 항상 '\0'이 들어감
+    // cout은 문자열을 출력하다가 NULL 문자를 만나면 출력을 중단함
+
+    //char str[255];
+    //cin.getline(str, 255);
+
+    //int ix = 0;
+    //while (true)
+    //{
+    //    if (str[ix] == '\0') break;
+
+    //    cout << str[ix] << ' ' << (int)str[ix] << endl;
+    //    ix++;
+    //}
+
+    //char source[] = "Copy this!";
+    //char dest[50];
+    //strcpy_s(dest, 50, source);
+
+    //cout << source << endl;
+    //cout << dest << endl;
+
+    // strcat() // 문자열 붙이기
+    // strcmp() // 문자열 비교
+
+    return 0;
+}
