19899f1369277c8bbdb2142299fd8267c2635062
pid011 <pid011@kakao.com>
2021-02-04 16:31:39 +0900

    feat: add chapter06_07_01

---
+++ src/chapter06_07_01/chapter06_07_01.cpp
@@ -0,0 +1,40 @@
+﻿// null pointer
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+void do_something(double* ptr)
+{
+    cout << "address of pointer variable in do_something() " << &ptr << endl;
+
+    if (ptr != nullptr)
+    {
+        // do something useful
+        cout << *ptr << endl;
+    }
+    else
+    {
+        // do nothing with ptr
+        cout << "Null ptr, do nothing" << endl;
+    }
+}
+
+int main()
+{
+    double* ptr1 = NULL;    // c-style
+    double* ptr2 = nullptr; // modern c++
+
+    do_something(ptr2);
+    do_something(nullptr);
+
+    double d { 123.4 };
+    do_something(&d);
+
+    ptr2 = &d;
+    do_something(ptr2);
+
+    std::nullptr_t nptr; // nullptr만 넣을 수 있음
+
+    cout << "address of pointer variable in main() " << &ptr2 << endl;
+}
