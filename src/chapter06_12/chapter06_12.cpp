971e95baa7f821e0590adb39f68b0deddb2378a2
pid011 <pid011@kakao.com>
2021-02-09 16:24:23 +0900

    feat: add chapter06_12

---
+++ src/chapter06_12/chapter06_12.cpp
@@ -0,0 +1,22 @@
+﻿// dynamically allocating arrays
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int len;
+    cin >> len;
+
+    int* arr = new int[len](); // 0으로 초기화
+
+    arr[0] = 1;
+    arr[1] = 2;
+
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << ' ' << uintptr_t(&arr[i]) << endl;
+    }
+
+    delete[] arr;
+}
