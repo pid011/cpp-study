20d759466ea0a230a5d202af7330cd250558a038
pid011 <pid011@kakao.com>
2021-03-30 18:35:57 +0900

    add chapter06_16

---
+++ src/chapter06_16/chapter06_16.cpp
@@ -0,0 +1,31 @@
+﻿// member selection operators
+#include <iostream>
+
+using namespace std;
+
+struct Person
+{
+    int age;
+    double weight;
+};
+
+int main()
+{
+    Person person;
+    person.age = 5;
+    person.weight = 30;
+
+    Person& ref = person;
+    ref.age = 15;
+
+    Person* ptr = &person;
+    ptr->age = 30;
+    // (*ptr).age = 20;
+
+    Person& ref2 = *ptr;
+    ref2.age = 45;
+
+    cout << &person << endl;
+    cout << &ref2 << endl;
+    // 똑같은 주소가 나온다.
+}
